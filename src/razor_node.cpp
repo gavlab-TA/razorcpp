ea26cdfbf9733cc05606338e2c3f664be5fbb799
Kyle Thompson <kat0028@auburn.edu>
2022-07-08 18:17:10 -0500

    intiial commit, package compiles and runs, no launch/config

--- src/razor_node.cpp
+++ src/razor_node.cpp
@@ -1,24 +1,33 @@
-#include <ros/ros.h>
-#include <sensor_msgs/Imu.h>
-#include <sensor_msgs/MagneticField.h>
-#include <sensor_msgs/Temperature.h>
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+#include "sensor_msgs/msg/temperature.hpp"
+#include "sensor_msgs/msg/magnetic_field.hpp"
 #include <razorcpp/razor_interface.h>
 
-class RazorNode
+class RazorNode : public rclcpp::Node
 {
     private:
-        ros::NodeHandle n;
-        ros::Publisher imu_pub;
-        ros::Publisher mag_pub;
-        ros::Publisher temp_pub;
+        rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub;
+        rclcpp::Publisher<sensor_msgs::msg::MagneticField>::SharedPtr mag_pub;
+        rclcpp::Publisher<sensor_msgs::msg::Temperature>::SharedPtr temp_pub;
 
-        sensor_msgs::Imu imu_msg;
-        sensor_msgs::MagneticField mag_msg;
-        sensor_msgs::Temperature temp_msg;
+        sensor_msgs::msg::Imu imu_msg;
+        sensor_msgs::msg::MagneticField mag_msg;
+        sensor_msgs::msg::Temperature temp_msg;
 
         RazorInterface* interface;
 
         RazorInterface::ImuData data;
+
+        rclcpp::TimerBase::SharedPtr timer;
+
+        void timerCallback()
+        {
+            if(interface->readMessage(data))
+            {
+                runPublishers();
+            }
+        }
 
         void runPublishers()
         {
@@ -28,42 +37,53 @@ class RazorNode
             imu_msg.linear_acceleration.x = data.accel.x;
             imu_msg.linear_acceleration.y = data.accel.y;
             imu_msg.linear_acceleration.z = data.accel.z;
-            imu_msg.header.stamp = ros::Time::now();
+            imu_msg.header.stamp = rclcpp::Clock().now();
             imu_msg.header.frame_id = "razor_imu";
 
             mag_msg.magnetic_field.x = data.mag.x;
             mag_msg.magnetic_field.y = data.mag.y;
             mag_msg.magnetic_field.z = data.mag.z;
-            mag_msg.header.stamp = ros::Time::now();
+            mag_msg.header.stamp = rclcpp::Clock().now();
             mag_msg.header.frame_id = "razor_mag";
 
             temp_msg.temperature = data.temp;
-            temp_msg.header.stamp = ros::Time::now();
+            temp_msg.header.stamp = rclcpp::Clock().now();
             temp_msg.header.frame_id = "razor_temp";
 
-            imu_pub.publish(imu_msg);
-            mag_pub.publish(mag_msg);
-            temp_pub.publish(temp_msg);
+            imu_pub->publish(imu_msg);
+            mag_pub->publish(mag_msg);
+            temp_pub->publish(temp_msg);
         }
 
     public:
-        RazorNode() : n("/test")
+        RazorNode() : Node("razor_node")
         {
+            this->declare_parameter<std::string>("imu_topic", "/razor/imu");
+            this->declare_parameter<std::string>("mag_topic", "/razor/mag");
+            this->declare_parameter<std::string>("temp_topic", "/razor/temperature");
+            this->declare_parameter<std::string>("port_name", "/dev/ttyUSB0");
+            this->declare_parameter<int>("baudrate", 115200);
+            this->declare_parameter<int>("timer_ms", 1);
+
             std::string imu_topic;
             std::string mag_topic;
             std::string temp_topic;
             std::string port_name;
             int baudrate;
+            int timer_ms;
 
-            n.param<std::string>("imu_topic", imu_topic, "/razor/imu");
-            n.param<std::string>("mag_topic", mag_topic, "/razor/mag");
-            n.param<std::string>("temp_topic", temp_topic, "/razor/temperature");
-            n.param<std::string>("port_name", port_name, "/dev/ttyUSB0");
-            n.param<int>("baudrate", baudrate, 115200);
+            this->get_parameter<std::string>("imu_topic", imu_topic);
+            this->get_parameter<std::string>("mag_topic", mag_topic);
+            this->get_parameter<std::string>("temp_topic", temp_topic);
+            this->get_parameter<std::string>("port_name", port_name);
+            this->get_parameter<int>("baudrate", baudrate);
+            this->get_parameter<int>("timer_ms", timer_ms);
 
-            imu_pub = n.advertise<sensor_msgs::Imu>(imu_topic, 1);
-            mag_pub = n.advertise<sensor_msgs::MagneticField>(mag_topic, 1);
-            temp_pub = n.advertise<sensor_msgs::Temperature>(temp_topic, 1);
+            this->imu_pub = this->create_publisher<sensor_msgs::msg::Imu>(imu_topic, 1);
+            this->mag_pub = this->create_publisher<sensor_msgs::msg::MagneticField>(mag_topic, 1);
+            this->temp_pub = this->create_publisher<sensor_msgs::msg::Temperature>(temp_topic, 1);
+            
+            this->timer = this->create_wall_timer(std::chrono::milliseconds(timer_ms), std::bind(&RazorNode::timerCallback, this));
 
             interface = new RazorInterface(port_name, baudrate);
         }
@@ -71,25 +91,14 @@ class RazorNode
         {
             delete interface;
         }
-        void readMessage()
-        {
-            if(interface->readMessage(data))
-            {
-                runPublishers();
-            }
-        }
+
+        
 };
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "razor_node");
-    RazorNode imu;
-    ros::Rate loop_rate(100000);
-    while (ros::ok())
-    {
-        imu.readMessage();
-        loop_rate.sleep();
-        ros::spinOnce();
-    }    
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<RazorNode>());
+    rclcpp::shutdown();
     return 0;
 }
